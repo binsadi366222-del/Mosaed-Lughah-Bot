a120167b86a95f526724a7e69f723234328692ea
binsadi366222-del <binsadi366222@gmail.com>
2026-09-11 18:10:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,11 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
         self.wfile.write(b"OK")
+        
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
 
     def log_message(self, format, *args):
         return  # إخفاء سجلات طلبات الفحص الدورية لعدم ملء الشاشة
